565fed5ce63d24f2581be6b3aaeacce677d773e7
August Sun <august99us@debianAugust.aug>
2015-11-24 00:14:44 -0500

    changed some routes

--- BilliardsAtNYU/routes/index.js
+++ BilliardsAtNYU/routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/tournament', function(req, res, next) {
+  res.render('tournament');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+router.post('/contact', function(req, res, next) {
+  // store contact logic
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  // logic to manage login
+})
+
+router.get('/logout', function(req, res, next) {
+  // logic to manage logout
+  res.render('logout');
+});
+
 module.exports = router;
