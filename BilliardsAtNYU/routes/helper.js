e06853411737e1f315e58d268ca9023758c42d91
August Sun <august99us@debianAugust.aug>
2015-12-07 01:19:51 -0500

    added database query helper method that should add a match with certain parameters to database

--- BilliardsAtNYU/routes/helper.js
+++ BilliardsAtNYU/routes/helper.js
@@ -13,6 +13,28 @@ function renderWithUser(req, res, route, data) {
   res.render(route, data);
 }
 
+/*
+function addMatch() {
+  // cypher query to add a match, with player 1, player 2 and attached to a specific tournament
+  db.cypher({
+    query: 'MATCH (a:Player {playername:{Player1Name}), (b:Player {playername: {Player2Name}),  (n:Tournament {semester: {TournamentName}})' +
+           'CREATE p =(a)-[:PLAYED_IN {playernumber: 1}]->(m:Match {raceto: {RaceTo}, player1score: {Player1Score}, player2score: {Player2Score}, games: {ProgressionOfGames}})<-[:PLAYED_IN {playernumber: 2}]-(b)' +
+           'CREATE (m)-[:PART_OF {roundof: {RoundOf}, matchnumber: {MatchNumber}}]->(n)',
+    params: {
+            Player1Name:
+            Player2Name:
+            TournamentName:
+            RaceTo:
+            Player1Score:
+            Player2Score:
+            ProgressionOfGames:
+            RoundOf:
+            MatchNumber:
+    }
+  })
+}
+*/
+
 helper = {};
 
 helper.renderWithUser = renderWithUser;
