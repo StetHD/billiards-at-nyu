89f28c041393a9f7a9de1d26899ca8f7cdd18768
August Sun <august99us@debianAugust.aug>
2015-12-10 20:43:01 -0500

    added new logic to player.js ajax to manage instance where not found

--- BilliardsAtNYU/public/javascripts/players.js
+++ BilliardsAtNYU/public/javascripts/players.js
@@ -25,33 +25,37 @@ function renderPlayer(result) {
     var oldContainer = document.getElementById("playerContainer");
     newContainer.id = oldContainer.id;
     
-    
-    var playerStats = document.createElement("div");
-    playerStats.classList.add("playerstats");
-    playerStats.innerHTML = "<h3>" + result.playerName + "</h3><p>Player Rank: " + (result.playerRank || "") + "</p>";
-    newContainer.appendChild(playerStats);
-    
-    var playerMatches = document.createElement("div");
-    playerMatches.classList.add("playermatches");
-    
-    var inner = document.createElement("div");
-    inner.classList.add("inner");
-    inner.innerHTML = "<h3>Recent Matches</h3>";
-    
-    for (i = 0; i < result.matches.length; i++) {
-        inner.innerHTML += "<hr class='lightcolor'> " +
-                           "Against: " + result.matches[i].opponent + " <br>" +
-                           "Score: (" + result.matches[i].playerScore + " - " + result.matches[i].opponentScore + ") (Player - Opponent) <br>" +
-                           "Date: " + (result.matches[i].date || "") + " <br>";
-        if (result.matches[i].isTournamentMatch) {
-            inner.innerHTML += "(Tournament Match) <br>" +
-                               "Tournament: " + result.matches[i].tournamentName + " <br>" +
-                               "Round Of: " + result.matches[i].roundOf + " <br>";
+    if (result.playerName) {
+        
+        var playerStats = document.createElement("div");
+        playerStats.classList.add("playerstats");
+        playerStats.innerHTML = "<h3>" + result.playerName + "</h3><p>Player Rank: " + (result.playerRank || "") + "</p>";
+        newContainer.appendChild(playerStats);
+        
+        var playerMatches = document.createElement("div");
+        playerMatches.classList.add("playermatches");
+        
+        var inner = document.createElement("div");
+        inner.classList.add("inner");
+        inner.innerHTML = "<h3>Recent Matches</h3>";
+        
+        for (i = 0; i < result.matches.length; i++) {
+            inner.innerHTML += "<hr class='lightcolor'> " +
+                               "Against: " + result.matches[i].opponent + " <br>" +
+                               "Score: (" + result.matches[i].playerScore + " - " + result.matches[i].opponentScore + ") (Player - Opponent) <br>" +
+                               "Date: " + (result.matches[i].date || "") + " <br>";
+            if (result.matches[i].isTournamentMatch) {
+                inner.innerHTML += "(Tournament Match) <br>" +
+                                   "Tournament: " + result.matches[i].tournamentName + " <br>" +
+                                   "Round Of: " + result.matches[i].roundOf + " <br>";
+            }
         }
+        
+        playerMatches.appendChild(inner);
+        newContainer.appendChild(playerMatches);    
+    } else {
+        newContainer.innerHTML = "Not found!";
     }
-    
-    playerMatches.appendChild(inner);
-    newContainer.appendChild(playerMatches);
     
     var parent = oldContainer.parentElement;
     parent.removeChild(oldContainer);
